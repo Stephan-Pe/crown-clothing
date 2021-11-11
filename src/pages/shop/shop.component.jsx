6dff075f138b20d35055ee98cb0c63a07cedb3b5
Stephan Petersen <54240006+Stephan-Pe@users.noreply.github.com>
2021-11-11 16:49:44 +0100

    useEffects on shop and app component to replace componentdidmount

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route } from "react-router-dom";
 
 import { connect } from "react-redux";
@@ -11,14 +11,11 @@ import CollectionsOverviewContainer from "../../components/collections-overview/
 import CollectionPageContainer from "../collection/collection.container";
 
 
-class ShopPage extends React.Component {
-  componentDidMount() {
-    const { fetchCollectionsStart } = this.props;
-    fetchCollectionsStart();
-  }
-
-  render() {
-    const { match } = this.props;
+const ShopPage = ({ fetchCollectionsStart, match } ) => {
+  useEffect(() => {
+     fetchCollectionsStart();
+  }, [fetchCollectionsStart])
+   
 
     return (
       <div className='shop-page'>
@@ -34,7 +31,7 @@ class ShopPage extends React.Component {
       </div>
     );
   }
-}
+
 
 
 const mapDispatchToProps = (dispatch) => ({
